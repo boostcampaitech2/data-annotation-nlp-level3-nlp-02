340ecf6be89c873ed2189e7d709c1e939f8b537e
sangha110495 <48673702+sangHa0411@users.noreply.github.com>
2021-11-19 11:46:45 +0900

    Add files via upload

--- load_data.py
+++ load_data.py
@@ -1,57 +1,115 @@
 import pickle as pickle
 import os
+import random
+import collections
 import pandas as pd
 import torch
+from torch.utils.data import Dataset, Subset
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
+class RE_Dataset(Dataset):
+    """ Dataset 구성을 위한 class."""
+    def __init__(self, pair_dataset, labels):
+        self.pair_dataset = pair_dataset
+        self.labels = labels
 
-class RE_Dataset(torch.utils.data.Dataset):
-  """ Dataset 구성을 위한 class."""
-  def __init__(self, pair_dataset, labels):
-    self.pair_dataset = pair_dataset
-    self.labels = labels
+    def __getitem__(self, idx):
+        item = {key: val[idx].clone().detach() for key, val in self.pair_dataset.items()}
+        item['labels'] = torch.tensor(self.labels[idx])
+        return item
 
-  def __getitem__(self, idx):
-    item = {key: val[idx].clone().detach() for key, val in self.pair_dataset.items()}
-    item['labels'] = torch.tensor(self.labels[idx])
-    return item
+    def __len__(self):
+        return len(self.labels)
 
-  def __len__(self):
-    return len(self.labels)
+    def split(self, val_ratio) :
+        data_size = len(self)
+        index_map = collections.defaultdict(list)
+        for idx in range(data_size) :
+            label = self.labels[idx]
+            index_map[label].append(idx)
+                
+        train_data = []
+        val_data = []
+            
+        label_size = len(index_map)
+        for label in range(label_size) :
+            idx_list = index_map[label]    
+            sample_size = int(len(idx_list) * val_ratio)
+
+            val_index = random.sample(idx_list, sample_size)
+            train_index = list(set(idx_list) - set(val_index))
+                
+            train_data.extend(train_index)
+            val_data.extend(val_index)
+            
+        random.shuffle(train_data)
+        random.shuffle(val_data)
+            
+        train_dset = Subset(self, train_data)
+        val_dset = Subset(self, val_data)
+        return train_dset, val_dset
 
 def preprocessing_dataset(dataset):
-  """ 처음 불러온 csv 파일을 원하는 형태의 DataFrame으로 변경 시켜줍니다."""
-  subject_entity = []
-  object_entity = []
-  for i,j in zip(dataset['subject_entity'], dataset['object_entity']):
-    i = i[1:-1].split(',')[0].split(':')[1]
-    j = j[1:-1].split(',')[0].split(':')[1]
+    """ 처음 불러온 csv 파일을 원하는 형태의 DataFrame으로 변경 시켜줍니다."""
+    subject_entity = [sub['text'] for sub in dataset.subject_entity.map(eval)]
+    object_entity = [sub['text'] for sub in dataset.object_entity.map(eval)]
+    out_dataset = pd.DataFrame({'id':dataset['id'], 'sentence':dataset['sentence'],'subject_entity':subject_entity,'object_entity':object_entity,'label':dataset['label'],})
+    return out_dataset
 
-    subject_entity.append(i)
-    object_entity.append(j)
-  out_dataset = pd.DataFrame({'id':dataset['id'], 'sentence':dataset['sentence'],'subject_entity':subject_entity,'object_entity':object_entity,'label':dataset['label'],})
-  return out_dataset
+def load_data(dataset):
+    """ csv 파일을 경로에 맡게 불러 옵니다. """
+    if os.path.isfile(dataset) :
+        pd_dataset = pd.read_csv(dataset)
+    else :
+        sheet_url = "https://docs.google.com/spreadsheets/d/1Zfhy3xh-2or5dUsSRm9d3yIN4Jhaleh32Rxqejy8hbI/edit#gid=0"
+        pd_dataset = load_googlesheet(sheet_url)
+        pd_dataset.to_csv(dataset)
+    
+    dataset = preprocessing_dataset(pd_dataset)
+    return dataset
 
-def load_data(dataset_dir):
-  """ csv 파일을 경로에 맡게 불러 옵니다. """
-  pd_dataset = pd.read_csv(dataset_dir)
-  dataset = preprocessing_dataset(pd_dataset)
-  
-  return dataset
+def load_googlesheet(spreadsheet_url) :
+    scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+    #json key file 위치
+    json_file_name = '/opt/ml/project/data/theta-webbing-298612-23be65a42b68.json'
+
+    # json key file을 이용하여 접속
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(json_file_name, scope)
+    gc = gspread.authorize(credentials)
+
+    # 스프레드시트 문서 가져오기
+    doc = gc.open_by_url(spreadsheet_url)
+
+    #시트 선택하기
+    sheet_name = "final"
+    worksheet = doc.worksheet(sheet_name)
+
+    values = worksheet.get_all_values()
+    header, rows = values[0], values[1:]
+
+    column_list = ["id","sentence","sentence_with_entity","subject_entity","object_entity","class"]
+    data = pd.DataFrame(rows, columns=header)
+    data = data[column_list]
+
+    target_data = data[["id", "sentence", "subject_entity", "object_entity", "class"]]
+    target_data.rename(columns = {"class" : "label"}, inplace = True)
+    return target_data
 
 def tokenized_dataset(dataset, tokenizer):
-  """ tokenizer에 따라 sentence를 tokenizing 합니다."""
-  concat_entity = []
-  for e01, e02 in zip(dataset['subject_entity'], dataset['object_entity']):
-    temp = ''
-    temp = e01 + '[SEP]' + e02
-    concat_entity.append(temp)
-  tokenized_sentences = tokenizer(
-      concat_entity,
-      list(dataset['sentence']),
-      return_tensors="pt",
-      padding=True,
-      truncation=True,
-      max_length=256,
-      add_special_tokens=True,
-      )
-  return tokenized_sentences
+    """ tokenizer에 따라 sentence를 tokenizing 합니다."""
+    concat_entity = []
+    for e01, e02 in zip(dataset['subject_entity'], dataset['object_entity']):
+        temp = ''
+        temp = e01 + '[SEP]' + e02
+        concat_entity.append(temp)
+    tokenized_sentences = tokenizer(
+        concat_entity,
+        list(dataset['sentence']),
+        return_tensors="pt",
+        padding=True,
+        truncation=True,
+        max_length=256,
+        add_special_tokens=True,
+        )
+    return tokenized_sentences
--- train.py
+++ train.py
@@ -8,20 +8,28 @@ from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_sc
 from transformers import AutoTokenizer, AutoConfig, AutoModelForSequenceClassification, Trainer, TrainingArguments, RobertaConfig, RobertaTokenizer, RobertaForSequenceClassification, BertTokenizer
 from load_data import *
 
+from dotenv import load_dotenv
+import wandb
 
 def klue_re_micro_f1(preds, labels):
     """KLUE-RE micro f1 (except no_relation)"""
-    label_list = ['no_relation', 'org:top_members/employees', 'org:members',
-       'org:product', 'per:title', 'org:alternate_names',
-       'per:employee_of', 'org:place_of_headquarters', 'per:product',
-       'org:number_of_employees/members', 'per:children',
-       'per:place_of_residence', 'per:alternate_names',
-       'per:other_family', 'per:colleagues', 'per:origin', 'per:siblings',
-       'per:spouse', 'org:founded', 'org:political/religious_affiliation',
-       'org:member_of', 'per:parents', 'org:dissolved',
-       'per:schools_attended', 'per:date_of_death', 'per:date_of_birth',
-       'per:place_of_birth', 'per:place_of_death', 'org:founded_by',
-       'per:religion']
+    label_list = [
+      'no_relation',
+      'product',
+      'location',
+      'poh:type',
+      'related',
+      'org:members',
+      'poh:start_date',
+      'per:member_of',
+      'alternate',
+      'org:field',
+      'org:event',
+      'per:title',
+      'org:founded',
+      'poh:end_date'
+    ]
+
     no_relation_label_idx = label_list.index("no_relation")
     label_indices = list(range(len(label_list)))
     label_indices.remove(no_relation_label_idx)
@@ -29,10 +37,10 @@ def klue_re_micro_f1(preds, labels):
 
 def klue_re_auprc(probs, labels):
     """KLUE-RE AUPRC (with no_relation)"""
-    labels = np.eye(30)[labels]
+    labels = np.eye(14)[labels]
 
-    score = np.zeros((30,))
-    for c in range(30):
+    score = np.zeros((14,))
+    for c in range(14):
         targets_c = labels.take([c], axis=1).ravel()
         preds_c = probs.take([c], axis=1).ravel()
         precision, recall, _ = sklearn.metrics.precision_recall_curve(targets_c, preds_c)
@@ -40,98 +48,118 @@ def klue_re_auprc(probs, labels):
     return np.average(score) * 100.0
 
 def compute_metrics(pred):
-  """ validation을 위한 metrics function """
-  labels = pred.label_ids
-  preds = pred.predictions.argmax(-1)
-  probs = pred.predictions
+    """ validation을 위한 metrics function """
+    labels = pred.label_ids
+    preds = pred.predictions.argmax(-1)
+    probs = pred.predictions
 
-  # calculate accuracy using sklearn's function
-  f1 = klue_re_micro_f1(preds, labels)
-  auprc = klue_re_auprc(probs, labels)
-  acc = accuracy_score(labels, preds) # 리더보드 평가에는 포함되지 않습니다.
+    # calculate accuracy using sklearn's function
+    f1 = klue_re_micro_f1(preds, labels)
+    auprc = klue_re_auprc(probs, labels)
+    acc = accuracy_score(labels, preds) # 리더보드 평가에는 포함되지 않습니다.
 
-  return {
-      'micro f1 score': f1,
-      'auprc' : auprc,
-      'accuracy': acc,
-  }
+    return {
+        'micro f1 score': f1,
+        'auprc' : auprc,
+        'accuracy': acc,
+    }
 
 def label_to_num(label):
-  num_label = []
-  with open('dict_label_to_num.pkl', 'rb') as f:
-    dict_label_to_num = pickle.load(f)
-  for v in label:
-    num_label.append(dict_label_to_num[v])
-  
-  return num_label
+    num_label = []
+    with open('dict_label_to_num.pkl', 'rb') as f:
+        dict_label_to_num = pickle.load(f)
+    for v in label:
+        num_label.append(dict_label_to_num[v])
+    
+    return num_label
 
 def train():
-  # load model and tokenizer
-  # MODEL_NAME = "bert-base-uncased"
-  MODEL_NAME = "klue/bert-base"
-  tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+    # load model and tokenizer
+    # MODEL_NAME = "bert-base-uncased"
+    MODEL_NAME = "klue/bert-base"
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+    dev_ratio = 0.2
 
-  # load dataset
-  train_dataset = load_data("../dataset/train/train.csv")
-  # dev_dataset = load_data("../dataset/train/dev.csv") # validation용 데이터는 따로 만드셔야 합니다.
+    # load dataset
+    dataset = load_data("./data/train/data.csv")
+    label = label_to_num(dataset['label'].values)
 
-  train_label = label_to_num(train_dataset['label'].values)
-  # dev_label = label_to_num(dev_dataset['label'].values)
+    # tokenizing dataset
+    dataset = tokenized_dataset(dataset, tokenizer)
 
-  # tokenizing dataset
-  tokenized_train = tokenized_dataset(train_dataset, tokenizer)
-  # tokenized_dev = tokenized_dataset(dev_dataset, tokenizer)
+    # make dataset for pytorch.
+    re_dataset = RE_Dataset(dataset, label)
+    re_train, re_dev = re_dataset.split(dev_ratio)
 
-  # make dataset for pytorch.
-  RE_train_dataset = RE_Dataset(tokenized_train, train_label)
-  # RE_dev_dataset = RE_Dataset(tokenized_dev, dev_label)
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-  device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    print(device)
+    # setting model hyperparameter
+    model_config =  AutoConfig.from_pretrained(MODEL_NAME)
+    model_config.num_labels = 14
 
-  print(device)
-  # setting model hyperparameter
-  model_config =  AutoConfig.from_pretrained(MODEL_NAME)
-  model_config.num_labels = 30
+    model =  AutoModelForSequenceClassification.from_pretrained(MODEL_NAME, config=model_config)
+    print(model.config)
+    model.parameters
+    model.to(device)
+    
+    # 사용한 option 외에도 다양한 option들이 있습니다.
+    # https://huggingface.co/transformers/main_classes/trainer.html#trainingarguments 참고해주세요.
+    training_args = TrainingArguments(
+        output_dir='./results',          # output directory
+        save_total_limit=5,              # number of total save model.
+        save_steps=140,                 # model saving step.
+        num_train_epochs=20,              # total number of training epochs
+        learning_rate=5e-5,               # learning_rate
+        per_device_train_batch_size=16,  # batch size per device during training
+        per_device_eval_batch_size=16,   # batch size for evaluation
+        warmup_steps=500,                # number of warmup steps for learning rate scheduler
+        weight_decay=0.01,               # strength of weight decay
+        logging_dir='./logs',            # directory for storing logs
+        logging_steps=100,              # log saving step.
+        evaluation_strategy='steps', # evaluation strategy to adopt during training
+                                     # `no`: No evaluation during training.
+                                     # `steps`: Evaluate every `eval_steps`.
+                                     # `epoch`: Evaluate every end of epoch.
+        eval_steps = 140,            # evaluation step.
+        load_best_model_at_end = True,
+        report_to="wandb"
+    )
+    trainer = Trainer(
+        model=model,                         # the instantiated 🤗 Transformers model to be trained
+        args=training_args,                  # training arguments, defined above
+        train_dataset=re_train,         # training dataset
+        eval_dataset=re_dev,            # evaluation dataset
+        compute_metrics=compute_metrics         # define metrics function
+    )
 
-  model =  AutoModelForSequenceClassification.from_pretrained(MODEL_NAME, config=model_config)
-  print(model.config)
-  model.parameters
-  model.to(device)
-  
-  # 사용한 option 외에도 다양한 option들이 있습니다.
-  # https://huggingface.co/transformers/main_classes/trainer.html#trainingarguments 참고해주세요.
-  training_args = TrainingArguments(
-    output_dir='./results',          # output directory
-    save_total_limit=5,              # number of total save model.
-    save_steps=500,                 # model saving step.
-    num_train_epochs=20,              # total number of training epochs
-    learning_rate=5e-5,               # learning_rate
-    per_device_train_batch_size=16,  # batch size per device during training
-    per_device_eval_batch_size=16,   # batch size for evaluation
-    warmup_steps=500,                # number of warmup steps for learning rate scheduler
-    weight_decay=0.01,               # strength of weight decay
-    logging_dir='./logs',            # directory for storing logs
-    logging_steps=100,              # log saving step.
-    evaluation_strategy='steps', # evaluation strategy to adopt during training
-                                # `no`: No evaluation during training.
-                                # `steps`: Evaluate every `eval_steps`.
-                                # `epoch`: Evaluate every end of epoch.
-    eval_steps = 500,            # evaluation step.
-    load_best_model_at_end = True 
-  )
-  trainer = Trainer(
-    model=model,                         # the instantiated 🤗 Transformers model to be trained
-    args=training_args,                  # training arguments, defined above
-    train_dataset=RE_train_dataset,         # training dataset
-    eval_dataset=RE_train_dataset,             # evaluation dataset
-    compute_metrics=compute_metrics         # define metrics function
-  )
+    # train model
+    trainer.train()
+    model.save_pretrained('./best_model')
 
-  # train model
-  trainer.train()
-  model.save_pretrained('./best_model')
 def main():
-  train()
+    load_dotenv(dotenv_path="./wandb.env")
+    WANDB_AUTH_KEY = os.getenv('WANDB_AUTH_KEY')
+    wandb.login(key=WANDB_AUTH_KEY)
+
+    wandb.init(
+        entity="klue-level2-nlp-02",
+        project="data-annotation", 
+        name='epoch-20_lr-5e-5',
+        group='klue/bert-base')
+    train()
+    wandb.finish()
+
+
+def seed_everything(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    np.random.seed(seed)
+    random.seed(seed)
 
 if __name__ == '__main__':
-  main()
+    seed_everything(42)
+    main()
